35eb812990a0de5b6ce77ea7a8dcb7e76601d6f6
rubrikteam <v.saikiran@hcltech.com>
2025-08-06 17:38:25 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,7 +1,1 @@
-# test
-
-import string
-text= "hellowsaikiraniam1993. my car blue"
-clean_text= text.translate(str.maketrans('','',string.punctuation))
-word_list= clean_text.split()
-print(word_list)
+print("hello world")
